6fbbbc3de84bdc982f6a5f2d610e0e7360fd00c2
foohwa <50357612+foohwa@users.noreply.github.com>
2024-10-15 15:01:44 +0800

    fix: include redirect and netlify toml config

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
+import { LoaderFunction, MetaFunction, redirect } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
+};
+
+export const loader: LoaderFunction = async () => {
+  return redirect("/reservations");
 };
 
 export default function Index() {
